af933a0111ec99985cbad0c2e5ce779fa4c7bf1e
Luiz Paulo Gonçalves Miguel de Jesus <luizpaulo.jesus@bmv.global>
2025-10-16 13:10:03 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/hooks/use-notifications.ts
+++ src/hooks/use-notifications.ts
@@ -88,7 +88,7 @@ export function NotificationProvider({ children }: { children: ReactNode }) {
     showError,
     showWarning,
     showInfo
-  }
+  };
 
   return (
     <NotificationContext.Provider value={contextValue}>
